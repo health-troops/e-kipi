9ae07458012d29c2f52bf34e9c09210930d2a77b
Lepiku <git.lepiku@gmail.com>
2021-05-19 20:50:15 +0700

    logout button

--- app/src/main/java/com/bangkit/healthtroops/ekipi/ui/home/HomeActivity.kt
+++ app/src/main/java/com/bangkit/healthtroops/ekipi/ui/home/HomeActivity.kt
@@ -1,15 +1,27 @@
 package com.bangkit.healthtroops.ekipi.ui.home
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bangkit.healthtroops.ekipi.R
 import com.bangkit.healthtroops.ekipi.databinding.ActivityHomeBinding
+import com.bangkit.healthtroops.ekipi.ui.auth.AuthActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
     private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +40,27 @@ class HomeActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navBar.setupWithNavController(navController)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_profile, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_logout -> {
+                sharedPref.edit {
+                    putString(AuthActivity.AUTH_EMAIL, null)
+                    val intent = Intent(this@HomeActivity, AuthActivity::class.java).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    }
+                    startActivity(intent)
+                }
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
