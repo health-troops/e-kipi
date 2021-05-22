8a79f5ee0e79657460e21b7d1a78b4e170872a66
Lepiku <git.lepiku@gmail.com>
2021-05-22 19:00:55 +0700

    create splash screen activity

---
+++ app/src/main/java/com/bangkit/healthtroops/ekipi/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,14 @@
+package com.bangkit.healthtroops.ekipi.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.healthtroops.ekipi.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+    }
+}
