64e6fa6f2ead936c0edef556ddecbaac9affe7e1
Lepiku <git.lepiku@gmail.com>
2021-05-22 19:07:09 +0700

    splash screen check for email

--- app/src/main/java/com/bangkit/healthtroops/ekipi/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/bangkit/healthtroops/ekipi/ui/splash/SplashScreenActivity.kt
@@ -1,14 +1,42 @@
 package com.bangkit.healthtroops.ekipi.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.healthtroops.ekipi.R
+import com.bangkit.healthtroops.ekipi.ui.auth.AuthActivity
+import com.bangkit.healthtroops.ekipi.ui.home.HomeActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SplashScreenActivity : AppCompatActivity() {
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
+
+        checkLoginStatus()
+    }
+
+    private fun checkLoginStatus() {
+        if (sharedPref.getString(AuthActivity.AUTH_EMAIL, null) != null) {
+            val intent = Intent(this, HomeActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+            startActivity(intent)
+        } else {
+            val intent = Intent(this, AuthActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+            startActivity(intent)
+        }
     }
 }
