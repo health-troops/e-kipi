eee1fa0b0a48ed728e3f504b1d7325f77960c519
Lepiku <git.lepiku@gmail.com>
2021-05-30 17:03:19 +0700

    sign out from settings

--- app/src/main/java/com/bangkit/healthtroops/ekipi/ui/home/HomeActivity.kt
+++ app/src/main/java/com/bangkit/healthtroops/ekipi/ui/home/HomeActivity.kt
@@ -1,28 +1,20 @@
 package com.bangkit.healthtroops.ekipi.ui.home
 
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.edit
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bangkit.healthtroops.ekipi.R
 import com.bangkit.healthtroops.ekipi.databinding.ActivityHomeBinding
-import com.bangkit.healthtroops.ekipi.ui.auth.AuthActivity
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
-    @Inject
-    lateinit var sharedPref: SharedPreferences
 
     private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -38,29 +30,5 @@ class HomeActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navigationBar.setupWithNavController(navController)
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_profile, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_logout -> {
-                sharedPref.edit {
-                    remove(AuthActivity.AUTH_EMAIL)
-                    remove(AuthActivity.AUTH_ID)
-                }
-                val intent = Intent(this@HomeActivity, AuthActivity::class.java).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                }
-                startActivity(intent)
-
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/bangkit/healthtroops/ekipi/ui/home/SettingsFragment.kt
+++ app/src/main/java/com/bangkit/healthtroops/ekipi/ui/home/SettingsFragment.kt
@@ -1,9 +1,12 @@
 package com.bangkit.healthtroops.ekipi.ui.home
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -11,8 +14,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.bangkit.healthtroops.ekipi.R
 import com.bangkit.healthtroops.ekipi.databinding.FragmentSettingsBinding
 import com.bangkit.healthtroops.ekipi.domain.model.ItemSelect
+import com.bangkit.healthtroops.ekipi.ui.auth.AuthActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment() {
+    @Inject
+    lateinit var sharedPref: SharedPreferences
 
     private var binding: FragmentSettingsBinding? = null
 
@@ -44,7 +53,15 @@ class SettingsFragment : Fragment() {
                 ItemSelect(
                     getString(R.string.sign_out), getString(R.string.sign_out_desc)
                 ) {
-                    // TODO sign out
+                    sharedPref.edit {
+                        remove(AuthActivity.AUTH_EMAIL)
+                        remove(AuthActivity.AUTH_ID)
+                    }
+                    val intent = Intent(requireActivity(), AuthActivity::class.java).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    }
+                    startActivity(intent)
                 },
             )
             rvItemSelect.apply {
