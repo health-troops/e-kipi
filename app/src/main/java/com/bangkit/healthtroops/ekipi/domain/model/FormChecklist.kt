f8adb15d1b04e66ed97693ab29e999973a38e4bd
Lepiku <git.lepiku@gmail.com>
2021-06-01 08:44:17 +0700

    fix warning in CameraActivity.kt

--- app/src/main/java/com/bangkit/healthtroops/ekipi/domain/model/FormChecklist.kt
+++ app/src/main/java/com/bangkit/healthtroops/ekipi/domain/model/FormChecklist.kt
@@ -1,7 +1,7 @@
 package com.bangkit.healthtroops.ekipi.domain.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class FormChecklist (
--- app/src/main/java/com/bangkit/healthtroops/ekipi/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/bangkit/healthtroops/ekipi/ui/camera/CameraActivity.kt
@@ -1,30 +1,28 @@
 package com.bangkit.healthtroops.ekipi.ui.camera
 
-import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.Color
-import android.net.Uri
+import android.graphics.ImageDecoder
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.healthtroops.ekipi.R
 import com.bangkit.healthtroops.ekipi.ml.FaceModel
 import org.tensorflow.lite.DataType
-import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 class CameraActivity : AppCompatActivity() {
-    private lateinit var select_image_button : Button
-    private lateinit var make_prediction : Button
-    private lateinit var img_view : ImageView
-    private lateinit var text_view : TextView
+    private lateinit var selectImageButton: Button
+    private lateinit var makePrediction: Button
+    private lateinit var imgView: ImageView
+    private lateinit var textView: TextView
     private lateinit var bitmap: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,82 +30,79 @@ class CameraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
-        select_image_button = findViewById(R.id.button)
-        make_prediction = findViewById(R.id.button2)
-        img_view = findViewById(R.id.imageView)
-        text_view = findViewById(R.id.textView)
+        selectImageButton = findViewById(R.id.button)
+        makePrediction = findViewById(R.id.button2)
+        imgView = findViewById(R.id.imageView)
+        textView = findViewById(R.id.textView)
 
-        val labels = application.assets.open("labels.txt").bufferedReader().use { it.readText() }.split("\n")
+        val labels =
+            application.assets.open("labels.txt").bufferedReader().use { it.readText() }.split("\n")
 
-        select_image_button.setOnClickListener(View.OnClickListener {
-            Log.d("mssg", "button pressed")
-            var intent : Intent = Intent(Intent.ACTION_GET_CONTENT)
-            intent.type = "image/*"
+        selectImageButton.setOnClickListener {
+            Log.d(TAG, "button pressed")
+            getContent.launch("image/*")
+        }
 
-            startActivityForResult(intent, 100)
-        })
-
-        make_prediction.setOnClickListener(View.OnClickListener {
-            var resized = Bitmap.createScaledBitmap(bitmap, 48, 48, true)
-            //var byteBuffer = getByteBuffer(resized)
-            var byteBuffer = ByteBuffer.allocateDirect(3*4 * 48*48).order(ByteOrder.nativeOrder())
-            val pixels = IntArray(48*48)
-            bitmap.getPixels(pixels, 0, 48, 0, 0, 48,48)
+        makePrediction.setOnClickListener {
+//            var byteBuffer = getByteBuffer(resized)
+            val byteBuffer =
+                ByteBuffer.allocateDirect(3 * 4 * 48 * 48).order(ByteOrder.nativeOrder())
+            val pixels = IntArray(48 * 48)
+            bitmap.getPixels(pixels, 0, 48, 0, 0, 48, 48)
             for (pixel in pixels) {
-                byteBuffer.putInt(pixel as Int)
+                byteBuffer.putInt(pixel)
             }
-
 
             val model = FaceModel.newInstance(this)
 
-//            var tbuffer = TensorImage.fromBitmap(resized)
-//            var byteBuffer = tbuffer.buffer
-//// Creates inputs for reference.
-//// Creates inputs for reference.
-            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1,48, 48, 3), DataType.FLOAT32)
+//            var tBuffer = TensorImage.fromBitmap(resized)
+//            var byteBuffer = tBuffer.buffer
+            // Creates inputs for reference.
+            val inputFeature0 =
+                TensorBuffer.createFixedSize(intArrayOf(1, 48, 48, 3), DataType.FLOAT32)
             inputFeature0.loadBuffer(byteBuffer)
 
-// Runs model inference and gets result.
+            // Runs model inference and gets result.
             val outputs = model.process(inputFeature0)
             val outputFeature0 = outputs.outputFeature0AsTensorBuffer
             println("================================================================")
             println(outputFeature0.floatArray)
-            var max = getMax(outputFeature0.floatArray)
+            val max = getMax(outputFeature0.floatArray)
             println(max)
 
-            text_view.text = labels[max]
+            textView.text = labels[max]
 
-// Releases model resources if no longer used.
+            // Releases model resources if no longer used.
             model.close()
-        })
-
-
+        }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        img_view.setImageURI(data?.data)
-
-        var uri : Uri ?= data?.data
-        bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
-
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        imgView.setImageURI(uri)
+        bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            ImageDecoder.decodeBitmap(ImageDecoder.createSource(this.contentResolver, uri))
+                .copy(Bitmap.Config.RGBA_F16, true)
+        } else {
+            MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+        }
     }
 
-    fun getMax(arr:FloatArray) : Int{
-        var ind = 0;
-        var min = 0.0f;
+    private fun getMax(arr: FloatArray): Int {
+        var ind = 0
+        var min = 0.0f
 
-        for(i in 0..6)
-        {   println("!!===================================!!")
+        for (i in 0..6) {
+            println("!!===================================!!")
             println(arr[i])
-            if(arr[i] > min)
-            {
+            if (arr[i] > min) {
                 min = arr[i]
-                ind = i;
+                ind = i
             }
         }
         return ind
     }
 
+    companion object {
+        private const val TAG = "CameraActivity"
+    }
 }
